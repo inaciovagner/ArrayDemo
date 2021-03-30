7b9e29eaa918871a1fae7a8d26433c680eb9abce
Vagner de Souza <inaciovagner1@gmail.com>
2021-03-30 12:55:22 +0100

    Add project files.

---
+++ ArrayDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Demo
+{
+    public class MyApplication
+    {
+        public static void Main(string[] args)
+        {
+            int[] list = { 98, 23, 97, 36, 77 };
+            Array.Sort(list);
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+        }
+    }
+}
